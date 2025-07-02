8b12631823e652754e81506d85bd3ca5b0b5f1ed
Teutenberg <>
2025-07-03 10:12:31 +1200

    lowercase for postgres

--- include/demo_db_create.sql
+++ include/demo_db_create.sql
@@ -10,14 +10,14 @@
 -- ============================================================================
 
 -- Create a new database named demo
-CREATE DATABASE DEMO;
+CREATE DATABASE demo;
 -- Connect to the demo database
 -- Note: The \connect command is specific to psql, the PostgreSQL command-line interface
-\connect DEMO
+\connect demo
 -- Create raw schema and a table in the demo database
-CREATE SCHEMA RAW;
-CREATE TABLE RAW.CUSTOMER (DATA JSONB NOT NULL);
-CREATE TABLE RAW.ACCOUNT (DATA JSONB NOT NULL);
-CREATE TABLE RAW.TRANSACTION (DATA JSONB NOT NULL);
+CREATE SCHEMA raw;
+CREATE TABLE raw.customer (data JSONB NOT NULL);
+CREATE TABLE raw.account (data JSONB NOT NULL);
+CREATE TABLE raw.transaction (data JSONB NOT NULL);
 
-CREATE SCHEMA STAGE;
+CREATE SCHEMA stage;
